e4c52000df9269dd8137de292ebc3d4dc2457f4b
szter <sthrpx.e@gmail.com>
2017-06-04 19:07:36 -0300

    Initial commit

--- js/app.js
+++ js/app.js
@@ -1,1 +1,26 @@
-// A module pattern should be used here
+$(document).ready(function() {
+  $('.btn, .btn-backspace, btn-clear').on('click', function() {
+    var $this = $(this);
+    var $thisValue = $(this).text();
+    if ($this.hasClass('btn-backspace')) {
+      var $num = $('.current').text();
+      var slice = $num.slice(0, -1);
+      $('.current').text(slice);
+    } else if ($this.hasClass('btn-clear')) {
+      $('.current').text('');
+      console.log('hola');
+    } else {
+      $('.current').append($thisValue);
+    }
+  });
+});
+
+var calc = (function() {
+    // Private variables and functions
+
+
+
+    // Public API
+    // return {
+    // };
+})();
